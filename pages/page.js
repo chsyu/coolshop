206a9bf70f44bfe03369e01b2441a1d45abe2531
Chi-Shan Yu <chisamyu@seed.net.tw>
2021-02-03 23:09:20 +0800

    page.js

---
+++ pages/page.js
@@ -0,0 +1,35 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Page() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Hello World!
+        </h1>
+
+        <p className={styles.description}>
+          This is a test book....
+        </p>
+
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  )
+}
